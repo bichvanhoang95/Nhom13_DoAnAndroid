fd13d8247b29e542b9d1f5ca623afe3358d3d4f1
bantaylua <viet.hung.tran.ngo@gmail.com>
2019-05-20 08:59:14 +0700

    updated

--- DACK_RentHouse/app/src/main/java/com/example/dack_renthouse/Utils.java
+++ DACK_RentHouse/app/src/main/java/com/example/dack_renthouse/Utils.java
@@ -3,7 +3,7 @@ package com.example.dack_renthouse;
 public class Utils {
 
     //Email Validation pattern
-    public static final String regEx = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}";
+    public static final String regEx = "\b [A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b";
 
     //Fragments Tags
     public static final String Login_Fragment = "Login_Fragment";
