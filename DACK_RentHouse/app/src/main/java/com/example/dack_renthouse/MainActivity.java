28da97d226be07e1fe00b245cfec59f8cce32f6d
bantaylua <viet.hung.tran.ngo@gmail.com>
2019-05-29 12:36:49 +0700

    Update MainActivity.java

--- DACK_RentHouse/app/src/main/java/com/example/dack_renthouse/MainActivity.java
+++ DACK_RentHouse/app/src/main/java/com/example/dack_renthouse/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private static FragmentManager fragmentManager;
